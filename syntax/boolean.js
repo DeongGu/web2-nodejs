7e5a1d1dab57c82fda71309b22ba2fec6d56730d
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-03-20 01:18:38 +0900

    syntax, nodejs base

---
+++ syntax/boolean.js
@@ -0,0 +1,8 @@
+console.log(true);
+console.log(false);
+console.log(1==1); //true
+console.log(1==2); //false
+console.log(1>2); //false
+console.log(1<2); //false
+console.log(1===1); //true
+console.log(1===2); //false
