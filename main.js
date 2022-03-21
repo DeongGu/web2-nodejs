ea937274da922c044823a637a6bb64f77811debf
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-03-21 19:03:57 +0900

    파일생성 및 리다이렉션

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
+var qs = require('querystring');
 
 function templateHTML(title, list, body) {
   return `
@@ -13,6 +14,7 @@ function templateHTML(title, list, body) {
   <body>
     <h1><a href="/">WEB</a></h1>
     ${list}
+    <a href="/create">create</a>
     ${body}
   </body>
   </html>
@@ -44,7 +46,7 @@ var app = http.createServer(function(request,response){
           const template = templateHTML(title, list, `<h2>${title}</h2>${description}`);
           response.writeHead(200);
           response.end(template);
-        })
+        });
       }else {
         fs.readdir("./data", function(error, fileList){
         fs.readFile(`data/${queryData.id}`, 'utf8', function(err, description){
@@ -56,7 +58,39 @@ var app = http.createServer(function(request,response){
         });
       });
       }
-}else {
+} else if(pathname === '/create'){
+  fs.readdir("./data", function(error, fileList){
+    var title = "WEB - create";
+    var list = templateList(fileList);
+    const template = templateHTML(title, list, `
+    <form action="http://localhost:3000/process_create" method="post">
+    <p><input type="text" name="title" placeholder="title"></p>
+    <p>
+        <textarea name="description" placeholder="description"></textarea>
+    </p>
+    <p>
+        <input type="submit">
+    </p>
+    </form>`);
+    response.writeHead(200);
+    response.end(template);
+  });
+} else if(pathname === '/process_create'){
+    var body = '';
+    request.on('data', function(data){
+      body = body + data;
+    });
+    request.on('end', function(){
+      var post = qs.parse(body);
+      var title = post.title;
+      var description = post.description;
+      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+        response.writeHead(302, {Location: `/?id=${title}`});
+        response.end('success');  
+      })
+    });
+    
+} else {
   response.writeHead(404);
   response.end('Not found');
 }
